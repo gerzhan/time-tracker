e3903869a7b478e145fa2d479afd1911113fca94
Ethan McGee <bulletshot60@gmail.com>
2013-11-20 17:20:30 -0500

    build schedule requests

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -1,24 +1,94 @@
+require 'date'
+
 class ScheduleController < ApplicationController
   
   def index
+    @requests = TimeRequest.where(user: current_user).order(:from)
   end
 
   def new
+    @time_request_types = TimeRequestType.order(:name)
   end
 
   def create
+    t = TimeRequest.new
+    t.user = current_user
+    t.name = params[:name]
+    t.comment = params[:comment]
+    t.time_request_type = params[:time_request_type] != "" ? TimeRequestType.find(params[:time_request_type]) : nil
+    begin
+      t.from = Date.strptime(params[:from], "%m/%d/%Y %H:%M")
+    rescue
+      t.from = Date.strptime(params[:from], "%m/%d/%Y")
+    end
+    begin
+      t.to = Date.strptime(params[:to], "%m/%d/%Y %H:%M")
+    rescue
+      t.to = Date.strptime(params[:to], "%m/%d/%Y")
+    end
+    t.save!
+
+    TimeRequestApprover.where(department: current_user.department, time_request_type: t.time_request_type).each do |approver|
+      r = TimeRequestApproval.new
+      r.time_request = t;
+      r.user = approver.user
+      r.approved = false
+      r.rejected = false
+      r.save!
+    end
+
+    flash[:success] = "Schedule Request Created"
+    redirect_to schedule_index_url
   end
 
   def show
+    @time_request = TimeRequest.find(params[:id])
   end
 
   def edit
+    @time_request = TimeRequest.find(params[:id])
+    @time_request_types = TimeRequestType.order(:name)
   end
 
   def update
+    t = TimeRequest.find(params[:id])
+    t.user = current_user
+    t.name = params[:name]
+    t.comment = params[:comment]
+    t.time_request_type = params[:time_request_type] != "" ? TimeRequestType.find(params[:time_request_type]) : nil
+    begin
+      t.from = Date.strptime(params[:from], "%m/%d/%Y %H:%M")
+    rescue
+      t.from = Date.strptime(params[:from], "%m/%d/%Y")
+    end
+    begin
+      t.to = Date.strptime(params[:to], "%m/%d/%Y %H:%M")
+    rescue
+      t.to = Date.strptime(params[:to], "%m/%d/%Y")
+    end
+    t.save!
+
+    t.time_request_approval.each do |r|
+      r.approved = false
+      r.rejected = false
+      r.save!
+    end
+
+    flash[:success] = "Schedule Request Updated"
+    redirect_to schedule_url(t.id)
   end
 
   def destroy
+    t = TimeRequest.find(params[:id])
+
+    t.time_request_approval.each do |r|
+      r.delete
+    end
+
+    t.delete
+
+    flash[:success] = "Schedule Request Deleted"
+    redirect_to schedule_index_url
   end
 
   def history
