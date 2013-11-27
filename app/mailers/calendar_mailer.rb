19870fe058a03b39fe4b944fca441f9e9a1d5d64
Ethan McGee <bulletshot60@gmail.com>
2013-11-27 16:18:51 -0500

    not a method

--- app/mailers/calendar_mailer.rb
+++ app/mailers/calendar_mailer.rb
@@ -5,8 +5,6 @@ class CalendarMailer < ActionMailer::Base
   def send_schedule_request(user, time_request)
     body = File.open("#{Rails.root}/public/schedule_requests/#{time_request.id}.ics","r").read
 
-    content_type "text/Calendar"
-
     mail(to: user.email,
          body: body,
          subject: "#{time_request.time_request_type ? time_request.time_request_type.name : ""} #{time_request.name}")
