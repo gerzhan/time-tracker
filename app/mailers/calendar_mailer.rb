262c20cc93f96264caaa20f02bafeddc7e915641
Ethan McGee <bulletshot60@gmail.com>
2013-11-27 19:00:09 -0500

    fix content-type

--- app/mailers/calendar_mailer.rb
+++ app/mailers/calendar_mailer.rb
@@ -1,15 +1,13 @@
 class CalendarMailer < ActionMailer::Base
-  default from: "system@time-tracker.com",
-  		  content_type: "text/calendar"
+  default from: "system@time-tracker.com"
 
   def send_schedule_request(user, time_request)
     body = File.open("#{Rails.root}/public/schedule_requests/#{time_request.id}.ics","r").read
 
-    headers['content-type'] = "text/calendar"
+    headers['Content-Type'] = "text/calendar"
 
     mail(to: user.email,
          body: body,
-         content_type: "text/calendar",
          subject: "#{time_request.time_request_type ? time_request.time_request_type.name : ""} #{time_request.name}")
   end
 end
